c5fd5e03e1be2362d2bc5d1f77687af45da58874
Tim-E <caffeinated.ink@gmail.com>
2024-03-06 16:24:15 -0500

    HTML me something, in-progress, first commit

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function init() {
+    // console.log("LOAD!!!");
+
+    document.getElementById('heart').addEventListener('click', function() {
+        // console.log('click');
+    });
+}
+
+window.addEventListener('load', init);
